79551af72c574e546cb14c0489e143e14098049a
Ulrike Boehm <ulrike.boehm@gmail.com>
2019-01-02 15:39:13 -0500

    Add files via upload
    New python code

---
+++ chapter3_statistics/frequency_table_sort.py
@@ -0,0 +1,19 @@
+"""
+Frequency table for a list of numbers
+Enhanced to display the table sorted by the numbers
+"""
+
+from collections import Counter
+
+def frequency_table(numbers):
+    table = Counter(numbers)
+    numbers_freq = table.most_common()
+    numbers_freq.sort()
+
+    print('Numbers\tFrequency')
+    for number in numbers_freq:
+        print('{0}\t{1}' .format(number[0], number[1]))
+
+if __name__ == '__main__':
+    scores = [7,8,9,2,10,9,9,9,9,4,5,6,1,5,6,7,8,6,1,10]
+    frequency_table(scores)

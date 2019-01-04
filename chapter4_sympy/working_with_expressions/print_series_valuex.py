12a5729ae47f09c04a6bd828be10d237cca9ef7c
Ulrike Boehm <ulrike.boehm@gmail.com>
2019-01-04 11:57:01 -0500

    Add files via upload
    upload python file

---
+++ chapter4_sympy/working_with_expressions/print_series_valuex.py
@@ -0,0 +1,36 @@
+"""
+
+Print the series:
+x + x**2 + x**3 + ... + x**n
+    ____   ____         ____
+     2      3            n
+
+and return the value of the series for a specific value of x
+
+"""
+
+from sympy import Symbol, pprint, init_printing
+
+def print_series(n, x_value):
+
+    # Initiate printing system with reverse order
+    init_printing(order='rev-lex')
+
+    x = Symbol('x')
+
+    series = x
+    for i in range(2,n+1):
+        series = series + (x**i)/i
+
+    pprint(series)
+
+    # Evaluate the series at x_value
+    series_value = series.subs({x:x_value})
+    print('Value of the series at {0}: {1}' .format(x_value, series_value))
+
+if __name__ == '__main__':
+
+    n = input("Enter the number of terms you want in the series: ")
+    x_value = input('Enter the value of x at which you want to evaluate the series: ')
+
+    print_series(int(n), float(x_value))

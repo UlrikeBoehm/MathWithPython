4f7aee22767ea7fb0d4b91fd66df729f676368a2
Ulrike Boehm <ulrike.boehm@gmail.com>
2019-01-04 15:46:38 -0500

    Add files via upload
    new python equation

---
+++ chapter4_sympy/plotting_using_sympy/plotting_user_input.py
@@ -0,0 +1,24 @@
+"""
+Plot the graph of an input expression
+"""
+
+from sympy import Symbol, sympify, solve, SympifyError
+from sympy.plotting import plot
+
+def plot_expression(expr):
+
+    y = Symbol('y')
+    solutions = solve(expr, y)
+    expr_y = solutions[0]
+    plot(expr_y)
+
+if __name__ == '__main__':
+
+    expr = input('Enter your expression in terms of x and y: ')
+
+    try:
+        expr = sympify(expr)
+    except SympifyError:
+        print('Invalid input')
+    else:
+        plot_expression(expr)

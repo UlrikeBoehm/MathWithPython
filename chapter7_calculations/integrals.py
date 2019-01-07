bad469d5c7b66572afb47494e966452981aa3932
Ulrike Boehm <ulrike.boehm@gmail.com>
2019-01-07 15:44:17 -0500

    Add files via upload
    new python file

---
+++ chapter7_calculations/integrals.py
@@ -0,0 +1,32 @@
+"""
+Finding the integrals of functions
+"""
+
+# Integral
+
+from sympy import Integral, Symbol
+x = Symbol('x')
+k = Symbol('k')
+Integral(k*x,x).doit()
+
+# Definite integral (lower and upper limits are specified)
+
+from sympy import Integral, Symbol
+x = Symbol('x')
+k = Symbol('k')
+Integral(k*x,(x,0,2)).doit()
+
+# probability density function
+
+from sympy import Symbol, exp, sqrt, pi, Integral
+x = Symbol('x')
+p = exp(-(x-10)**2/2)/sqrt(2*pi)
+Integral(p,(x,11,12)).doit().evalf() # Use evalf() to calculate a numeric value
+
+# probability density function (check: normalization)
+
+from sympy import Symbol, exp, sqrt, pi, Integral, S
+x = Symbol('x')
+p = exp(-(x-10)**2/2)/sqrt(2*pi)
+Integral(p,(x,S.NegativeInfinity,S.Infinity)).doit().evalf() # Use evalf() to calculate a numeric value
+# Result should be 1!
